6ceab2505f5974af4817ebf80e24184eea924363
Robert Nazaryan <robertnazaryan0@gmail.com>
2024-04-16 21:03:50 +0400

    lesson 3

---
+++ src/main/java/org/example/catalogservice/domain/BookNotFoundException.java
@@ -0,0 +1,9 @@
+package org.example.catalogservice.domain;
+
+public class BookNotFoundException extends RuntimeException {
+
+    public BookNotFoundException(String isbn) {
+        super("The book with ISBN " + isbn + " was not found.");
+    }
+
+}
